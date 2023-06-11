93cce8af3e3b6bffac6d4e920248fdb554f604fb
Ryota <ryoenoki2013@gmail.com>
2023-06-11 16:24:30 +0900

    using js inside component

--- section3/src/App.js
+++ section3/src/App.js
@@ -4,6 +4,8 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- section3/src/components/ExpenseItem.js
+++ section3/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 5, 11);
+  const expenseTitle = "Title";
+  const expenseAmount = 100;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
